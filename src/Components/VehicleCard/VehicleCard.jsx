c37a6370e4d4ddb1465ea50b3644a685c805cfe4
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-01-08 22:58:16 +0530

    Vehicle image was added

--- src/Components/VehicleCard/VehicleCard.jsx
+++ src/Components/VehicleCard/VehicleCard.jsx
@@ -1,10 +1,46 @@
 
-function VehicleCard({name}) {
+// function VehicleCard({name, image, vehicle_no}) {
+//   return (
+//     <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[500px] bg-slate-300 flex  m-2 rounded-md relative">
+//        <div>
+//         <img src={image} className="w-[97%] h-[35%] m-2 flex absolute rounded-md"/>
+//        </div>
+//        <div className=" bg-green-300 flex flex-col absolute rounded-md top-[200px] left-[10px] p-1">
+//               <h2>{vehicle_no}</h2>
+//        </div>
+//     </div>
+//   )
+// }
+
+// export default VehicleCard
+
+
+function VehicleCard({ name, image, vehicle_no, model, station, category}) {
   return (
-    <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[500px] bg-slate-300 flex p-2 m-2 rounded-md">
-        {name}
+    <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[500px] bg-slate-300 flex m-2 rounded-md relative">
+      <div>
+        <img
+          src={image}
+          className="w-[97%] h-[35%] m-2 flex absolute rounded-md"
+          alt="Vehicle"
+        />
       </div>
-  )
+      <div className="bg-green-400 flex flex-col justify-center items-center absolute rounded-md top-[200px] left-[180px] p-1 w-[24%]">
+        <h2 className="text-center text-md font-semibold">{vehicle_no}</h2>
+      </div>
+     <div className="flex flex-row ">
+     <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[250px] justify-center p-1 m-2">
+             <h2 className="text-center text-md font-semibold">{model}</h2>
+      </div>
+      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[250px] justify-center p-1 m-2 mx-[180px]">
+             <h2 className="text-center text-md font-semibold">{station}</h2>
+      </div>
+      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[250px] justify-center p-1 m-2 mx-[330px]">
+             <h2 className="text-center text-md font-semibold">{category}</h2>
+      </div>
+     </div>
+    </div>
+  );
 }
 
-export default VehicleCard
+export default VehicleCard;
--- src/Components/VehicleCard/VehiclePanel.jsx
+++ src/Components/VehicleCard/VehiclePanel.jsx
@@ -23,9 +23,9 @@ import VehicleCard from "./VehicleCard";
 function VehiclePanel() {
   return (
     <div className="flex flex-wrap justify-center gap-4">
-      <VehicleCard name={'Vehicle 1'}/>
-      <VehicleCard name={'Vehicle 2'}/>
-     <VehicleCard name={'vehicle 3'}/>
+      <VehicleCard name={'Vehicle 1'} station={'Veyangoda'} category={'Electric'} model={'Nissan Leaf'} vehicle_no={'wp-2693'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
+      <VehicleCard name={'Vehicle 2'} station={'Paliyagoda'} category={'Petrol'} model={'Prado'} vehicle_no={'wp-2163'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
+      <VehicleCard name={'vehicle 3'} station={'Kiribathgoda'} category={'Hybrid'} model={'Monterro'} vehicle_no={'wp-5679'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
     </div>
   );
 }
