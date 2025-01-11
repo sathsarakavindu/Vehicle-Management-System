ae36b817427652477da24a8a09b5fbc1f6623812
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-01-11 18:31:48 +0530

    fetched json data

--- src/Components/VehicleCard/VehicleCard.jsx
+++ src/Components/VehicleCard/VehicleCard.jsx
@@ -1,89 +1,154 @@
 
-// function VehicleCard({name, image, vehicle_no}) {
+// function VehicleCard({ name, image, vehicle_no, model, station, category, chassyNumber, engineNumber, revenueLicenseNumber, manufactureYear, province, policeOfficer, temporaryLocation, isActive, isInPoliceGarage, fundAmount}) {
 //   return (
-//     <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[500px] bg-slate-300 flex  m-2 rounded-md relative">
-//        <div>
-//         <img src={image} className="w-[97%] h-[35%] m-2 flex absolute rounded-md"/>
-//        </div>
-//        <div className=" bg-green-300 flex flex-col absolute rounded-md top-[200px] left-[10px] p-1">
-//               <h2>{vehicle_no}</h2>
-//        </div>
+//     <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[500px] bg-slate-300 flex m-2 rounded-md relative">
+//       <div>
+//         <img
+//           src={image}
+//           className="w-[97%] h-[35%] m-2 flex absolute rounded-md"
+//           alt="Vehicle"
+//         />
+//       </div>
+//       <div className="bg-green-400 flex flex-col justify-center items-center absolute rounded-md top-[200px] left-[180px] p-1 w-[24%]">
+//         <h2 className="text-center text-md font-semibold">{vehicle_no}</h2>
+//       </div>
+//      <div className="flex flex-row ">
+//      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[240px] justify-center p-2 m-2">
+//              <h2 className="text-center text-md font-semibold">{name}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[240px] justify-center p-2 m-2 mx-[180px]">
+//              <h2 className="text-center text-md font-semibold">{station}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[240px] justify-center p-2 m-2 mx-[330px]">
+//              <h2 className="text-center text-md font-semibold">{category}</h2>
+//       </div>
+      
+//      </div>
+//      {/* second line */}
+    
+//      <div className="flex flex-row ">
+//      <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[290px] justify-center p-2 m-2">
+//              <h2 className="text-center text-md font-semibold">{chassyNumber}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[290px] justify-center p-2 m-2 mx-[180px]">
+//              <h2 className="text-center text-md font-semibold">{engineNumber}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[290px] justify-center p-2 m-2 mx-[330px]">
+//              <h2 className="text-center text-md font-semibold">{revenueLicenseNumber}</h2>
+//       </div>
+//      </div>
+
+//        {/* third line */}
+    
+//        <div className="flex flex-row ">
+//      <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[365px] justify-center p-2 m-2">
+//              <h2 className="text-center text-md font-semibold">{manufactureYear}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[365px] justify-center p-2 m-2 mx-[180px]">
+//              <h2 className="text-center text-md font-semibold">{province}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[365px] justify-center p-2 m-2 mx-[330px]">
+//              <h2 className="text-center text-md font-semibold">{policeOfficer}</h2>
+//       </div>
+//      </div>
+
+//         {/* fourth line */}
+    
+//         <div className="flex flex-row ">
+//      <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[440px] justify-center p-1 m-2">
+//              <h2 className="text-center text-md font-semibold">{temporaryLocation}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[440px] justify-center p-2 m-2 mx-[180px]">
+//              <h2 className="text-center text-md font-semibold">{isActive ? "Active" : "Not Active"}</h2>
+//       </div>
+//       <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[440px] justify-center p-2 m-2 mx-[330px]">
+//              <h2 className="text-center text-md font-semibold">{fundAmount}</h2>
+//       </div>
+//      </div>
+     
 //     </div>
-//   )
+//   );
 // }
 
-// export default VehicleCard
+// export default VehicleCard;
 
 
 function VehicleCard({ name, image, vehicle_no, model, station, category, chassyNumber, engineNumber, revenueLicenseNumber, manufactureYear, province, policeOfficer, temporaryLocation, isActive, isInPoliceGarage, fundAmount}) {
-  return (
-    <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[500px] bg-slate-300 flex m-2 rounded-md relative">
-      <div>
-        <img
-          src={image}
-          className="w-[97%] h-[35%] m-2 flex absolute rounded-md"
-          alt="Vehicle"
-        />
-      </div>
-      <div className="bg-green-400 flex flex-col justify-center items-center absolute rounded-md top-[200px] left-[180px] p-1 w-[24%]">
-        <h2 className="text-center text-md font-semibold">{vehicle_no}</h2>
-      </div>
-     <div className="flex flex-row ">
-     <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[240px] justify-center p-2 m-2">
-             <h2 className="text-center text-md font-semibold">{model}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[240px] justify-center p-2 m-2 mx-[180px]">
-             <h2 className="text-center text-md font-semibold">{station}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[240px] justify-center p-2 m-2 mx-[330px]">
-             <h2 className="text-center text-md font-semibold">{category}</h2>
-      </div>
-      
-     </div>
-     {/* second line */}
-    
-     <div className="flex flex-row ">
-     <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[290px] justify-center p-2 m-2">
-             <h2 className="text-center text-md font-semibold">Chassy: {chassyNumber}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[290px] justify-center p-2 m-2 mx-[180px]">
-             <h2 className="text-center text-md font-semibold">Eng: {engineNumber}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[290px] justify-center p-2 m-2 mx-[330px]">
-             <h2 className="text-center text-md font-semibold">Rev: {revenueLicenseNumber}</h2>
-      </div>
-     </div>
+       return (
+         <div className="w-full sm:w-full md:w-[48%] lg:w-[30%] h-[980px] bg-slate-300 flex m-2 rounded-md relative">
+           <div>
+             <img
+               src={image}
+               className="w-[97%] h-[38%] m-2 flex absolute rounded-md"
+               alt="Vehicle"
+             />
+           </div>
+         
+         <div className="bg-green-400 flex flex-row justify-center items-center absolute rounded-md top-[380px] mx-2 p-1 w-[97%]">
+             <h2 className="text-center text-sm ">{vehicle_no}</h2>
+         </div>
+          <div className="flex">
 
-       {/* third line */}
-    
-       <div className="flex flex-row ">
-     <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[365px] justify-center p-2 m-2">
-             <h2 className="text-center text-md font-semibold">Manufactured Year: {manufactureYear}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[365px] justify-center p-2 m-2 mx-[180px]">
-             <h2 className="text-center text-md font-semibold">Province: {province}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[365px] justify-center p-2 m-2 mx-[330px]">
-             <h2 className="text-center text-md font-semibold">{policeOfficer}</h2>
-      </div>
-     </div>
+          <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[410px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{name}</h2>
+           </div>
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[450px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">Chassy Number: {chassyNumber}</h2>
+           </div>
 
-        {/* fourth line */}
-    
-        <div className="flex flex-row ">
-     <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[440px] justify-center p-1 m-2">
-             <h2 className="text-center text-md font-semibold">{temporaryLocation}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[25%] top-[440px] justify-center p-2 m-2 mx-[180px]">
-             <h2 className="text-center text-md font-semibold">{isActive}</h2>
-      </div>
-      <div className="bg-blue-300 flex absolute rounded-md w-[24%] top-[440px] justify-center p-2 m-2 mx-[330px]">
-             <h2 className="text-center text-md font-semibold">{fundAmount}</h2>
-      </div>
-     </div>
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[490px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">Engine Number: {engineNumber}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[530px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">Revenue Licence Number: {revenueLicenseNumber}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[570px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">Manufacured Year: : {manufactureYear}</h2>
+           </div>
+           
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[610px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">Provice: : {province}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[650px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{temporaryLocation}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[690px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{policeOfficer}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[730px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{station}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[770px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{isInPoliceGarage ? "Available" : "Not Available"}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[810px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{isActive ? "Active" : "Not Active"}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[850px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{category}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[890px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{model}</h2>
+           </div>
+
+           <div className="bg-blue-300 flex absolute rounded-md w-[97%] top-[930px] justify-center p-1 m-2">
+                  <h2 className="text-center text-sm">{fundAmount}</h2>
+           </div>
+
+           </div>
+          
+         </div>
+       );
+     }
      
-    </div>
-  );
-}
-
-export default VehicleCard;
+     export default VehicleCard;
+     
--- src/Components/VehicleCard/VehiclePanel.jsx
+++ src/Components/VehicleCard/VehiclePanel.jsx
@@ -15,18 +15,54 @@
 //   )
 // }
 
-import VehicleCard from "./VehicleCard";
-
 // export default VehicleCard
 
+import axios from "axios";
+import VehicleCard from "./VehicleCard";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export const getVehicles = async () => {
+  const response = await axios.get('/api/vehicles');
+  return response.data.list;
+};
 
 function VehiclePanel() {
+  const [vehicle, setVehicle] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+   useEffect(() => {
+        if(!isLoading){
+          const fetchVehicles = async () => {
+            try {
+              const data = await getVehicles();
+              setVehicle(data); 
+              console.log(data);
+              setLoading(true);
+            } catch (error) {
+              console.error("Error fetching vehicles:", error);
+            }
+          };
+          fetchVehicles();
+        }
+        
+  }, [isLoading]);
+  
+
   return (
+    // <div className="flex flex-wrap justify-center gap-4">
+    //   <VehicleCard fundAmount={0} isInPoliceGarage={'True'} isActive={'True'} temporaryLocation={'Service Center'} policeOfficer={'Jane Smith'} province={'Western'} manufactureYear={'2020'} revenueLicenseNumber={'RL123456'} engineNumber={'ENG987654321'} chassyNumber={'CHS123456789'} name={'Vehicle 1'} station={'Veyangoda'} category={'Electric'} model={'Nissan Leaf'} vehicle_no={'wp-2693'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
+    //   <VehicleCard fundAmount={100} isInPoliceGarage={'True'} isActive={'False'} temporaryLocation={'Service Center'} policeOfficer={'David Camarron'} province={'Western'} manufactureYear={'2020'} revenueLicenseNumber={'RL123456'} engineNumber={'ENG987654321'} chassyNumber={'CHS123456789'} name={'Vehicle 2'} station={'Paliyagoda'} category={'Petrol'} model={'Prado'} vehicle_no={'wp-2163'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
+    //   <VehicleCard fundAmount={500} isInPoliceGarage={'False'} isActive={'True'} temporaryLocation={'Service Center'} policeOfficer={'Edward Martin'} province={'Western'} manufactureYear={'2018'} revenueLicenseNumber={'RL123456'} engineNumber={'ENG987654321'} chassyNumber={'CHS123456789'} name={'vehicle 3'} station={'Kiribathgoda'} category={'Hybrid'} model={'Monterro'} vehicle_no={'wp-5679'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
+    // </div>
     <div className="flex flex-wrap justify-center gap-4">
-      <VehicleCard fundAmount={0} isInPoliceGarage={'True'} isActive={'True'} temporaryLocation={'Service Center'} policeOfficer={'Jane Smith'} province={'Western'} manufactureYear={'2020'} revenueLicenseNumber={'RL123456'} engineNumber={'ENG987654321'} chassyNumber={'CHS123456789'} name={'Vehicle 1'} station={'Veyangoda'} category={'Electric'} model={'Nissan Leaf'} vehicle_no={'wp-2693'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
-      <VehicleCard fundAmount={100} isInPoliceGarage={'True'} isActive={'False'} temporaryLocation={'Service Center'} policeOfficer={'David Camarron'} province={'Western'} manufactureYear={'2020'} revenueLicenseNumber={'RL123456'} engineNumber={'ENG987654321'} chassyNumber={'CHS123456789'} name={'Vehicle 2'} station={'Paliyagoda'} category={'Petrol'} model={'Prado'} vehicle_no={'wp-2163'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
-      <VehicleCard fundAmount={500} isInPoliceGarage={'False'} isActive={'True'} temporaryLocation={'Service Center'} policeOfficer={'Edward Martin'} province={'Western'} manufactureYear={'2018'} revenueLicenseNumber={'RL123456'} engineNumber={'ENG987654321'} chassyNumber={'CHS123456789'} name={'vehicle 3'} station={'Kiribathgoda'} category={'Hybrid'} model={'Monterro'} vehicle_no={'wp-5679'} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
-    </div>
+       {
+        vehicle.map((value, index)=>(
+          <VehicleCard key={index} fundAmount={value.fundAmount} isInPoliceGarage={value.isInPoliceGarage} isActive={value.isActive} temporaryLocation={value.temporaryLocation} policeOfficer={value.policeOfficer} province={value.province} manufactureYear={value.manufactureYear} revenueLicenseNumber={value.revenueLicenseNumber} engineNumber={value.engineNumber} chassyNumber={value.chassyNumber} name={value.vehicleName} station={value.policeStation
+          } category={value.vehicleCategory} model={value.vehicleBrand} vehicle_no={value.vehicleNumber} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7P4uDsjBCS_f54801NJpbGxzVgcgq20vcQIgfjb23U11mDoue0xML-p4&s'}/>
+        ))
+       }
+  </div>
   );
 }
 
