ee972df51f55bc5ff37f00c60818294a8cef616d
Shanka Visal <shankavisal@gmail.com>
2025-01-18 01:45:10 +0530

    add about and contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Login from './Pages/Admin/login';
 import ManageVehicle from './Pages/Admin/ManageVehicle';
+import AboutUs from './Pages/About';
+import ContactUs from './Pages/Contact';
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
         <Route path="/*" element={<HomePage/>}/>
         <Route path="/manage-vehicle" element={<ManageVehicle/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path='/about' element={<AboutUs/>}/>
+        <Route path='/contact' element={<ContactUs/>}/>
       </Routes>
     </BrowserRouter>
    </div>
